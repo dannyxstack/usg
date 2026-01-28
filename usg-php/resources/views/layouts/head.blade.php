253d02c88b1be2ea828f8f3898365bfcba5796c8
danny <danielxu1100@gmail.com>
2026-01-28 20:46:19 +0800

    update

--- usg-php/resources/views/layouts/head.blade.php
+++ usg-php/resources/views/layouts/head.blade.php
@@ -10,8 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">  <!-- 你的自定义 CSS -->
+    <link href="/css/style.css" rel="stylesheet">  <!-- 你的自定义 CSS -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('js/script.js') }}"></script>
+    <script src="/js/script.js"></script>
 </head>
