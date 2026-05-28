25ce8888b1fcf007ed339bbd924cc1673b93ba60
danny <danielxu1100@gmail.com>
2026-05-28 18:08:24 +0800

    update admin

--- usg-php/app/Filament/Resources/Users/Schemas/UserForm.php
+++ usg-php/app/Filament/Resources/Users/Schemas/UserForm.php
@@ -5,6 +5,10 @@ namespace App\Filament\Resources\Users\Schemas;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Hash;
+
+use Filament\Forms;
+use Filament\Forms\Components\Select;
 
 class UserForm
 {
@@ -21,7 +25,17 @@ class UserForm
                 DateTimePicker::make('email_verified_at'),
                 TextInput::make('password')
                     ->password()
-                    ->required(),
+                    // ->required(),
+                    ->required(fn (string $operation): bool => $operation === 'create')  // 创建时必须填
+                    ->dehydrated(fn ($state) => filled($state))  // 只有当密码字段有值时，才将其提交保存
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state))  // 提交前自动加密
+                    ,
+                Select::make('roles')
+                    ->relationship('roles', 'name')
+                    ->multiple()
+                    ->preload()
+                    ->searchable()
+                    ->label('Role'),
             ]);
     }
 }
